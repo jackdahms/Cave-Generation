2e7f999dcf873407f25b6a555070dd2d96fb8434
Deco <jack.dahms@gmail.com>
2016-04-09 00:19:17 -0500

    began work on presets

--- src/com/jackdahms/CaveGenerator.java
+++ src/com/jackdahms/CaveGenerator.java
@@ -4,25 +4,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class CaveGenerator extends JPanel implements KeyListener{
-	
+		
 	static int FRAME_WIDTH = 1000;
 	static int FRAME_HEIGHT = 600;
 	
-	int width = 200;
-	int height = 150;
-	int cellSize = 4;
-	int borderWidth = 4;
-	int smoothingIterations = 5;
-	
-	Cave cave;
-	
+	List<Cave> presets = new ArrayList<Cave>();
+				
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -41,31 +36,29 @@ public class CaveGenerator extends JPanel implements KeyListener{
 	}
 	
 	public CaveGenerator() {
-		cave = new Cave(width, height);
-		generate();
+		Cave cave = new Cave(200, 150);
 	}
 	
-	//TODO make cave a buffered image
 	@Override
 	public void paintComponent(Graphics g) {
-		for (int r = 0; r < cave.height; r++) {
-			for (int c = 0; c < cave.width; c++) {
-				Color col;
-				if (cave.map[r][c] == 1) col = Color.black;
-				else col = Color.white;
-				g.setColor(col);
-				g.fillRect(c * cellSize, r * cellSize, cellSize, cellSize);
-			}
-		}
+//		g.drawImage(cave.image, 0, 0, 800, 600, null);
 		
 		g.setColor(Color.white);
 		g.fillRect(800, 0, 200, 600);
+		
+		g.setColor(Color.black);
 	}
 	
 	void generate() {
-		cave.fillMap(0.5f);
-		cave.fillBorder(borderWidth);
-		for (int i = 0; i < smoothingIterations; i++) cave.simpleSmoothMap();
+//		for (int i = 0; i < smoothingIterations; i++) cave.simpleSmoothMap(() -> {
+//			for (int r = 0; r < height; r++) {
+//				for (int c = 0; c < width; c++) {
+//					if (cave.countSurroundingWalls(r, c) < 4) cave.map[r][c] = 0;
+//					else if (cave.countSurroundingWalls(r, c) > 4) cave.map[r][c] = 1;
+//				}
+//			}
+//			return cave.map;
+//		});
 	}
 
 	@Override
