e09a78ce0716eed131ceec16138d8307a689e107
jack <jack.dahms@gmail.com>
2016-04-21 08:29:08 -0500

    created ruleset

---
+++ src/com/jackdahms/Ruleset.java
@@ -0,0 +1,16 @@
+package com.jackdahms;
+
+import java.util.List;
+
+public class Ruleset {
+	
+	String name;
+	float fillDensity;
+	Generatable mapper;
+	List<Generatable> smoothing;
+	
+	public Ruleset(String name, float fillDensity, Generatable mapper) {
+		
+	}
+
+}
