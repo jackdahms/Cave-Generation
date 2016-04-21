ba5a82645debcd01232f8d7a034a8414398ce6bd
jack <jack.dahms@gmail.com>
2016-04-21 07:22:14 -0500

    added iterate once button

--- src/com/jackdahms/CaveGenerator.java
+++ src/com/jackdahms/CaveGenerator.java
@@ -18,6 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerListModel;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SpringLayout;
@@ -32,7 +33,8 @@ public class CaveGenerator extends JPanel implements KeyListener{
 	static int FRAME_WIDTH = 1000;
 	static int FRAME_HEIGHT = 600;
 
-	List<Cave> presets = new ArrayList<Cave>();
+	List<String> presetNames = new ArrayList<String>();
+	List<Generatable> presets = new ArrayList<Generatable>();
 	
 	Cave cave;
 	
@@ -59,6 +61,8 @@ public class CaveGenerator extends JPanel implements KeyListener{
 	}
 	
 	public CaveGenerator() {
+		createPresets();
+		
 		generate();
 		
 		createGUI();
@@ -81,18 +85,9 @@ public class CaveGenerator extends JPanel implements KeyListener{
 	
 	void generate() {
 		cave = new Cave(width, height);
-		int smoothingIterations = 5;
 		cave.fillMap(fillDensity);
 		cave.fillBorder(2);
-		for (int i = 0; i < smoothingIterations; i++) cave.simpleSmoothMap(() -> {
-			for (int r = 0; r < height; r++) {
-				for (int c = 0; c < width; c++) {
-					if (cave.countSurroundingWalls(r, c) < 4) cave.map[r][c] = 0;
-					else if (cave.countSurroundingWalls(r, c) > 4) cave.map[r][c] = 1;
-				}
-			}
-			return cave.map;
-		});
+		for (int i = 0; i < smoothingIterations; i++) cave.smooth(presets.get(0));
 		cave.mapToImage();
 		repaint();
 	}
@@ -106,6 +101,19 @@ public class CaveGenerator extends JPanel implements KeyListener{
 	@Override
 	public void keyReleased(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_ENTER) generate();
+	}
+	
+	private void createPresets() {
+		presetNames.add("default");
+		presets.add(() -> {
+			for (int r = 0; r < height; r++) {
+				for (int c = 0; c < width; c++) {
+					if (cave.countSurroundingWalls(r, c) < 4) cave.map[r][c] = 0;
+					else if (cave.countSurroundingWalls(r, c) > 4) cave.map[r][c] = 1;
+				}
+			}
+			return cave.map;
+		});
 	}
 	
 	private void createGUI() {
@@ -138,12 +146,12 @@ public class CaveGenerator extends JPanel implements KeyListener{
 		
 		JButton fillDensityButton = new JButton("Fill New Cave");
 		
+		JSpinner rulesetSpinner = new JSpinner();
 		JLabel iterationsLabel = new JLabel("Smoothing Iterations");
 		JSpinner iterationsSpinner = new JSpinner();
 		JSlider iterationsSlider = new JSlider();
 		JButton iterateButton = new JButton("Iterate Once"); //for iterating once
 		JButton smoothButton = new JButton("Smooth Existing Map"); //do all iterations
-		
 		
 		JButton regenerateButton = new JButton("Generate New Cave");
 		
@@ -279,6 +287,8 @@ public class CaveGenerator extends JPanel implements KeyListener{
 		layout.putConstraint(SpringLayout.EAST, fillDensityButton, -5, SpringLayout.EAST, this);
 		add(fillDensityButton);
 		
+		SpinnerListModel listModel = new SpinnerListModel(presetNames);
+		
 		numberModel = new SpinnerNumberModel();
 		numberModel.setMinimum(1);
 		numberModel.setMaximum(20);
@@ -309,6 +319,15 @@ public class CaveGenerator extends JPanel implements KeyListener{
 		
 		iterateButton.setPreferredSize(new Dimension(190, 30));
 		iterateButton.addActionListener((ActionEvent e) -> System.out.println("once"));
+		layout.putConstraint(SpringLayout.NORTH, iterateButton, 5, SpringLayout.SOUTH, iterationsSlider);
+		layout.putConstraint(SpringLayout.WEST, iterateButton, -195, SpringLayout.EAST, this);
+		add(iterateButton);
+		
+		smoothButton.setPreferredSize(new Dimension(190, 30));
+		smoothButton.addActionListener((ActionEvent e) -> System.out.println("smooth"));
+		layout.putConstraint(SpringLayout.NORTH, smoothButton, 5, SpringLayout.SOUTH, iterateButton);
+		layout.putConstraint(SpringLayout.WEST, smoothButton, -195, SpringLayout.EAST, this);
+		add(smoothButton);
 		
 		regenerateButton.setPreferredSize(new Dimension(190, 30));
 		regenerateButton.addActionListener((ActionEvent e) -> generate());
